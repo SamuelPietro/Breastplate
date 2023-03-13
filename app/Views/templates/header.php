5be1aa943450684c8a61a7ab1dbd979647c908f4
Samuel Pietro <samuelpietro.b@gmail.com>
2023-03-13 16:24:51 -0300

    feat: ready for translations

--- app/Views/templates/header.php
+++ app/Views/templates/header.php
@@ -1,21 +1,21 @@
 <!doctype html>
-<html lang="pt-BR">
+<html lang="<?= getenv('LANG'); ?>">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-    <meta name="title" content="<?= $_ENV['APP_NAME'] ?>">
-    <meta name="description" content="<?= $_ENV['APP_DESC'] ?>">
-    <meta name="keywords" content="<?= $_ENV['APP_KEYS'] ?>">
+    <meta name="title" content="<?= getenv('APP_NAME') ?>">
+    <meta name="description" content="<?= getenv('APP_DESC'); ?>">
+    <meta name="keywords" content="<?= getenv('APP_KEYS'); ?>">
     <meta name="robots" content="index, follow">
     <meta name="language" content="Portuguese">
     <meta name="revisit-after" content="7 days">
-    <meta name="author" content="<?= $_ENV['APP_AUTHOR'] ?>">
-    <link rel="icon" type="image/x-icon" href="<?= $_ENV['BASE_URL'] ?>public/images/favicon.ico" />
-    <title><?= $_ENV['APP_NAME'] ?></title>
+    <meta name="author" content="<?= getenv('APP_AUTHOR'); ?>">
+    <link rel="icon" type="image/x-icon" href="<?= getenv('BASE_URL'); ?>/images/favicon.ico" />
+    <title><?= getenv('APP_NAME'); ?></title>
     <!-- Styles -->
-    <link rel="stylesheet" href="<?= $_ENV['BASE_URL'] ?>public/css/style.css">
+    <link rel="stylesheet" href="<?= getenv('BASE_URL'); ?>public/css/style.css">
     <!-- Scripts -->
-    <script src="<?= $_ENV['BASE_URL'] ?>public/js/script.js" defer></script>
+    <script src="<?= getenv('BASE_URL'); ?>public/js/script.js" defer></script>
 </head>
 <body>
