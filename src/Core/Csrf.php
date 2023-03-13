a8dd8c4e1bdd9146b08c5142d83604d24355eaff
Samuel Pietro <samuelpietro.b@gmail.com>
2023-03-13 15:05:29 -0300

    feat: created class to handle CSRF

---
+++ src/Core/Csrf.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Src\Core;
+
+use Exception;
+
+class Csrf
+{
+    private mixed $token;
+
+    /**
+     * @throws Exception
+     */
+    public function __construct()
+    {
+        if (!isset($_SESSION['csrf_token'])) {
+            $this->token = bin2hex(random_bytes(32));
+            $_SESSION['csrf_token'] = $this->token;
+        } else {
+            $this->token = $_SESSION['csrf_token'];
+        }
+    }
+
+    public function generate(): string
+    {
+        return '<input type="hidden" name="csrf_token" value="' . $this->token . '">';
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function verify(): bool
+    {
+        if (!isset($_POST['csrf_token']) || $_POST['csrf_token'] !== $this->token) {
+            error_log(gettext('Invalid CSRF Token'));
+        }
+        return true;
+    }
+}
