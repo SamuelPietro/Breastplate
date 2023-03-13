be48fecb00fdc2e561114b1883dc78e2124e08ad
Samuel Pietro <samuelpietro.b@gmail.com>
2023-03-13 14:58:33 -0300

    fix: exceptions will now throw error_log
    Removal of routes is temporary. In the future we intend to return with the routes to usersController that were removed

--- src/Core/Routes.php
+++ src/Core/Routes.php
@@ -35,19 +35,12 @@ class Routes
         // Define routes for AppController.
         $this->router->addRoute('GET', '/', 'AppController', 'index');
 
-        // Define routes for UsersController.
-        $this->router->addRoute('GET', '/users', 'UsersController', 'index');
-        $this->router->addRoute('GET', '/users/show/{id}', 'UsersController', 'show');
-        $this->router->addRoute('GET', '/users/create', 'UsersController', 'create');
-        $this->router->addRoute('POST', '/users/store', 'UsersController', 'store');
-        $this->router->addRoute('GET', '/users/edit/{id}', 'UsersController', 'edit');
-        $this->router->addRoute('POST', '/users/update/{id}', 'UsersController', 'update');
-        $this->router->addRoute('POST', '/users/delete/{id}', 'UsersController', 'delete');
-
         // Define routes for AuthController.
         $this->router->addRoute('GET', '/login', 'AuthController', 'login');
         $this->router->addRoute('POST', '/login', 'AuthController', 'login');
         $this->router->addRoute('GET', '/logout', 'AuthController', 'logout');
+
+
     }
 
     /**
@@ -59,13 +52,14 @@ class Routes
      */
     public function run(): void
     {
+
         $method = $_SERVER['REQUEST_METHOD'];
         $path = $_SERVER['REQUEST_URI'];
 
         try {
             $this->router->route($method, $path);
         } catch (Exception $e) {
-            throw new Exception("Error getting routes: " . $e->getMessage());
+            error_log(gettext('Error getting routes') . $e->getMessage());
         }
     }
 }
