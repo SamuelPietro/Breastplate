f4236f17a39a26a820391f26a8a68568c72914bd
Samuel Barbosa <samuel@educarbrasil.org.br>
2024-02-17 00:50:20 -0300

    refactor: Move container creation and bootstrap initialization to separate functions for improved readability and future extensibility"

--- public/index.php
+++ public/index.php
@@ -20,9 +20,32 @@ use Src\Core\Bootstrap;
 require_once __DIR__ . '/../vendor/autoload.php';
 
 try {
-    $container = new Container();
-    $bootstrap = $container->get(Bootstrap::class);
+    $container = createContainer();
+    $bootstrap = createBootstrap($container);
     $bootstrap->init();
 } catch (Exception $exception) {
     error_log('Error executing the application: ' . $exception->getMessage());
+}
+
+/**
+ * Creates a new instance of the dependency injection container.
+ *
+ * @return Container The dependency injection container.
+ * @throws Exception If an error occurs while creating the container.
+ */
+function createContainer(): Container
+{
+    return new Container();
+}
+
+/**
+ * Creates a new instance of the Bootstrap class.
+ *
+ * @param Container $container The dependency injection container.
+ * @return Bootstrap The Bootstrap class instance.
+ * @throws Exception If an error occurs while creating the Bootstrap instance.
+ */
+function createBootstrap(Container $container): Bootstrap
+{
+    return new Bootstrap($container);
 }
