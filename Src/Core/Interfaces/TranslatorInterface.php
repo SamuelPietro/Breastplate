7f168d28e390c2c225492a70b096c1e9a91bd2ca
Samuel Pietro <samuelpietro.b@gmail.com>
2025-04-11 14:26:28 -0300

    docs: add comments and documentation to the Translator class and TranslatorInterface interface

--- Src/Core/Interfaces/TranslatorInterface.php
+++ Src/Core/Interfaces/TranslatorInterface.php
@@ -2,8 +2,28 @@
 
 namespace Breastplate\Src\Core\Interfaces;
 
+/**
+ * TranslatorInterface interface
+ * 
+ * This interface defines the methods for a translator.
+ * 
+ * 
+ */
 interface TranslatorInterface
 {
+    /**
+     * Translate a message.
+     * 
+     * @param string $key The key to translate.
+     * @return string The translated message.
+     */
     public function trans(string $key): string;
+
+    /**
+     * Set the locale.
+     * 
+     * @param string $locale The locale to use.
+     * @return void
+     */
     public function setLocale(string $locale): void;
 }
--- Src/Core/Translator.php
+++ Src/Core/Translator.php
@@ -4,17 +4,35 @@ namespace Breastplate\Src\Core;
 
 use Breastplate\Src\Core\Interfaces\TranslatorInterface;
 
+/**
+ * Translator class
+ * 
+ * This class is responsible for translating messages.
+ * 
+ * 
+ */
 class Translator implements TranslatorInterface
 {
     private string $locale;
     private array $translations = [];
 
+    /**
+     * Constructor.
+     * 
+     * @param string $locale The locale to use.
+     * @return void
+     */
     public function __construct(string $locale = 'en')
     {
         $this->locale = $locale;
         $this->loadTranslations();
     }
 
+    /**
+     * Load translations from the language file.
+     * 
+     * @return void
+     */
     private function loadTranslations(): void
     {
         $file = __DIR__ . '/../../resources/lang/' . $this->locale . '.php';
@@ -23,11 +41,23 @@ class Translator implements TranslatorInterface
         }
     }
 
+    /**
+     * Translate a message.
+     * 
+     * @param string $key The key to translate.
+     * @return string The translated message.
+     */
     public function trans(string $key): string
     {
         return $this->translations[$key] ?? $key;
     }
 
+    /**
+     * Set the locale.
+     * 
+     * @param string $locale The locale to use. 
+     * @return void
+     */
     public function setLocale(string $locale): void
     {
         $this->locale = $locale;
